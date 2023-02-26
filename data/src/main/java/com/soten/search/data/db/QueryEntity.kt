201d44745fe600ed01118484e2ac3ce0bab7fbbf
Choiyounho <maxosa78@gmail.com>
2023-02-27 03:17:38 +0900

    refactor : entity 프라이머리키 변경

--- data/src/main/java/com/soten/search/data/db/QueryEntity.kt
+++ data/src/main/java/com/soten/search/data/db/QueryEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class QueryEntity(
-    val query: String,
     @PrimaryKey
-    val createAt: Long? = null
+    val query: String,
+    val createAt: Long
 )
