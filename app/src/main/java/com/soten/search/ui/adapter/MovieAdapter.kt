ac9d90175dfc8ba9d9ee206f5396a2ae7d3a5cb2
Choiyounho <maxosa78@gmail.com>
2023-02-27 01:42:30 +0900

    feat : 영화 어댑터 구현

---
+++ app/src/main/java/com/soten/search/ui/adapter/MovieAdapter.kt
@@ -0,0 +1,31 @@
+package com.soten.search.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.soten.search.domain.MovieDomain
+
+class MovieAdapter(
+    private val onClick: (MovieDomain) -> Unit
+) : ListAdapter<MovieDomain, MovieViewHolder>(movieDiffer) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return MovieViewHolder.create(parent, onClick)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
+    }
+
+    companion object {
+        private val movieDiffer = object : DiffUtil.ItemCallback<MovieDomain>() {
+            override fun areItemsTheSame(oldItem: MovieDomain, newItem: MovieDomain): Boolean {
+                return oldItem.image == newItem.image
+            }
+
+            override fun areContentsTheSame(oldItem: MovieDomain, newItem: MovieDomain): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
