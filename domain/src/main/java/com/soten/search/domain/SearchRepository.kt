9616f682843bcf8223a5042bbb0d6956fbcc0ab5
Choiyounho <maxosa78@gmail.com>
2023-02-26 23:59:34 +0900

    refactor : 레퍼지토리 함수 suspend 변경

--- domain/src/main/java/com/soten/search/domain/SearchRepository.kt
+++ domain/src/main/java/com/soten/search/domain/SearchRepository.kt
@@ -4,7 +4,7 @@ interface SearchRepository {
 
     suspend fun fetchSearchMovies(query: String, start: Int = DEFAULT_START): List<MovieDomain>
 
-    fun insertMovie(movieDomain: MovieDomain)
+    suspend fun insertMovie(movieDomain: MovieDomain)
 
     companion object {
         private const val DEFAULT_START = 1
