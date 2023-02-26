b93da7fff97646b7cedab7bca0d8edffbd5f0865
Choiyounho <maxosa78@gmail.com>
2023-02-27 02:30:28 +0900

    refactor : addAll 확장함수 제거

--- app/src/main/java/com/soten/search/extension/FlowExtension.kt
+++ app/src/main/java/com/soten/search/extension/FlowExtension.kt
@@ -1,17 +1,7 @@
 package com.soten.search.extension
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.update
-
-fun <T> MutableStateFlow<List<T>>.addAll(list: List<T>) {
-    update {
-        val currentList = it.toMutableList()
-        currentList.addAll(list)
-        currentList.toList()
-    }
-}
 
 fun <T> Flow<T>.throttleFirst(windowDuration: Long): Flow<T> = flow {
     var lastEmissionTime = 0L
