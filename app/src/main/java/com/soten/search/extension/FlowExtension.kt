892915c816b537bca823583e89a8c8f15fffd13a
Choiyounho <maxosa78@gmail.com>
2023-02-27 01:41:24 +0900

    feat : 클릭 쓰로틀 코드 구현

--- app/src/main/java/com/soten/search/extension/FlowExtension.kt
+++ app/src/main/java/com/soten/search/extension/FlowExtension.kt
@@ -1,6 +1,8 @@
 package com.soten.search.extension
 
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.update
 
 fun <T> MutableStateFlow<List<T>>.addAll(list: List<T>) {
@@ -8,5 +10,16 @@ fun <T> MutableStateFlow<List<T>>.addAll(list: List<T>) {
         val currentList = it.toMutableList()
         currentList.addAll(list)
         currentList.toList()
+    }
+}
+
+fun <T> Flow<T>.throttleFirst(windowDuration: Long): Flow<T> = flow {
+    var lastEmissionTime = 0L
+    collect { upstream ->
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - lastEmissionTime > windowDuration) {
+            lastEmissionTime = currentTime
+            emit(upstream)
+        }
     }
 }
---
+++ app/src/main/java/com/soten/search/extension/ViewExteinsion.kt
@@ -0,0 +1,15 @@
+package com.soten.search.extension
+
+import android.view.View
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+@ExperimentalCoroutinesApi
+fun View.clicks(): Flow<Unit> = callbackFlow {
+    setOnClickListener {
+        trySend(Unit)
+    }
+    awaitClose { setOnClickListener(null) }
+}
