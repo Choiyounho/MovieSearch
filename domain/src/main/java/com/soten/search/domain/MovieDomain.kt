a01943e1471fda2c8dcabccc42f264fc3a767643
Choiyounho <maxosa78@gmail.com>
2023-02-26 23:21:16 +0900

    feat : MovieDomain 구현

---
+++ domain/src/main/java/com/soten/search/domain/MovieDomain.kt
@@ -0,0 +1,9 @@
+package com.soten.search.domain
+
+data class MovieDomain(
+    val title: String,
+    val link: String,
+    val image: String,
+    val pubDate: String,
+    val userRating: String
+)
