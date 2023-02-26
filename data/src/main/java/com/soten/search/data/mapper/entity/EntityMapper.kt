a070f0157c6529d0f04793f00d383bb2aa30e559
Choiyounho <maxosa78@gmail.com>
2023-02-27 00:16:42 +0900

    docs : kotlin 추가

--- data/src/main/java/com/soten/search/data/mapper/entity/EntityMapper.kt
+++ data/src/main/java/com/soten/search/data/mapper/entity/EntityMapper.kt
@@ -2,8 +2,6 @@ package com.soten.search.data.mapper.entity
 
 import com.soten.search.data.db.QueryEntity
 import com.soten.search.data.mapper.Mapper
-import com.soten.search.data.network.response.MovieDto
-import com.soten.search.domain.MovieDomain
 import javax.inject.Inject
 
 internal class EntityMapper @Inject constructor(): Mapper<QueryEntity, String> {
